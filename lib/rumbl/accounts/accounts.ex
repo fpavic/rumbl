f68c3e1dfaa488cbe7181ffb62846af6db1d4f78
Filip Pavic <pavicfp@gmail.com>
2019-04-05 16:25:16 +0200

    Crud operations on user

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -2,24 +2,32 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context.
   """
-
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
   def list_users do
-    [
-      %User{id: "1", name: "José", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
+    Repo.all(User)
   end
 
   def get_user(id) do
-    Enum.find(list_users(), &(&1.id == id))
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {k, v} -> Map.get(map, k) == v end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
   end
 end
