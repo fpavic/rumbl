797225e8d8c50a756cca2a40e4819df70b81e697
Filip Pavic <pavicfp@gmail.com>
2019-04-08 12:12:41 +0200

    Finish sessions

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -4,6 +4,28 @@ defmodule Rumbl.Accounts do
   """
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
+  alias Rumbl.Accounts.Credential
+
+  import Ecto.Query
+
+  def authenticate_by_email_and_password(email, given_pass) do
+    user = get_user_by_email(email)
+
+    cond do
+      user && Comeonin.Pbkdf2.checkpw(given_pass, user.credential.password_hash) ->
+        {:ok, user}
+      user ->
+        {:error, :unauthorized}
+      true -> Comeonin.Bcrypt.dummy_checkpw()
+        {:error, :not_found}
+    end
+  end
+
+  def get_user_by_email(email) do
+    from(u in User, join: c in assoc(u, :credential), where: c.email == ^email)
+    |> Repo.one()
+    |> Repo.preload(:credential)
+  end
 
   def list_users do
     Repo.all(User)
@@ -29,5 +51,37 @@ defmodule Rumbl.Accounts do
     %User{}
     |> User.changeset(attrs)
     |> Repo.insert()
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def list_credentials do
+    Repo.all(Credential)
+  end
+
+  def get_credential!(id), do: Repo.get!(Credential, id)
+
+  def create_credential(attrs \\ %{}) do
+    %Credential{}
+    |> Credential.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_credential(%Credential{} = credential, attrs) do
+    credential
+    |> Credential.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_credential(%Credential{} = credential) do
+    Repo.delete(credential)
+  end
+
+  def change_credential(%Credential{} = credential) do
+    Credential.changeset(credential, %{})
   end
 end
