e187436bb02e6df49b33eba2dc9cb46addf9921b
Filip Pavic <pavicfp@gmail.com>
2019-04-18 18:01:50 +0200

    fully implemeted sockets

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -12,6 +12,8 @@ defmodule Rumbl.Multimedia.Video do
     belongs_to :user, Rumbl.Accounts.User
     belongs_to :category, Rumbl.Multimedia.Category
 
+    has_many :annotations, Rumbl.Multimedia.Annotation, on_delete: :delete_all
+
     timestamps()
   end
 
