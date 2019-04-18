9ecac3b3a7cad3684d1c19427c8ef7fac8955488
Filip Pavic <pavicfp@gmail.com>
2019-04-18 16:07:32 +0200

    authenticate socket connection

--- lib/rumbl_web/plugs/auth.ex
+++ lib/rumbl_web/plugs/auth.ex
@@ -12,9 +12,11 @@ defmodule RumblWeb.Auth do
 
     cond do
       user = conn.assigns[:current_user] ->
-        conn
+        put_current_user(conn, user)
+
       user = user_id && Accounts.get_user(user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
+
       true ->
         assign(conn, :current_user, nil)
     end
@@ -33,7 +35,7 @@ defmodule RumblWeb.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -48,5 +50,13 @@ defmodule RumblWeb.Auth do
 
   def logout(conn) do
     configure_session(conn, drop: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 end
